13232121a1e2f5517aff437ce13751655e7f45ff
Alicia Resendez <AAALICIA2004@YAHOO.COM>
2014-06-26 11:25:51 -0500

    created routes for resume and porfolio

--- app/routes.php
+++ app/routes.php
@@ -11,7 +11,29 @@
 |
 */
 
-Route::get('/', function()
+Route::get('/resume', function()
 {
-	return View::make('hello');
+	return "This is my resume!";
 });
+
+Route::get('/portfolio', function()
+{
+	return "This is my portfolio!";
+});
+
+// Route::get('/', function (){
+// 	return 'we are home';
+// });
+
+// Route::get('/sayhello/{name}', function($name)
+// {
+// 	if ($name == "Alicia")
+//     {
+//         return Redirect::to('/'); //this Redirect will take you to the home page. the / means home page
+//         //return Redirect::to('http://www.google.com'); this Redirect can take you to any locatin you want to go
+//     }
+//     else
+//     {
+//         return "Hello, $name!";
+//     }
+// });
